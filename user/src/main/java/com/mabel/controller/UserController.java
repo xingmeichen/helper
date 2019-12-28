2b91e164cef8e00d281e590690a17faf09c8c420
xingmeichen <xingmeichen12@126.com>
2019-12-28 21:55:38 +0800

    update:update port

--- user/src/main/java/com/mabel/controller/UserController.java
+++ user/src/main/java/com/mabel/controller/UserController.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
