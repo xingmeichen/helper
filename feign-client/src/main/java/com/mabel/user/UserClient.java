38e9739664ba1f13e5ce85dba63fd846ef0edff7
xingmeichen <xingmeichen12@126.com>
2020-07-18 12:49:47 +0800

    update: remove no use import

--- feign-client/src/main/java/com/mabel/user/UserClient.java
+++ feign-client/src/main/java/com/mabel/user/UserClient.java
@@ -4,7 +4,6 @@ import com.mabel.pojo.model.user.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
